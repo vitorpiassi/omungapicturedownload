7481aa00bc975af572022e35d7618b9f015020b0
Renie Siqueira <renie.webdev@gmail.com>
2016-08-29 22:43:31 -0300

    Estrutura

---
+++ utils/connection.php
@@ -0,0 +1,12 @@
+<?php
+ error_reporting(E_ALL); ini_set('display_errors', 1);
+
+if ( ($handle = fopen("dbconf", "r") ) !== FALSE ) {
+	
+	$data = fgetcsv($handle, 1050, ",");
+    
+    $db = mysqli_connect($data[0], $data[1], $data[2], $data[3]);
+
+    fclose($handle);
+}
+?>

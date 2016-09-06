335f4e2e008cfed7f86dbe0a97620371a8575157
Vitor Piassi <vitorpiassi@gmail.com>
2016-09-06 00:42:49 -0300

    Adicionando fotos e implementando o download e envio de email

--- index.php
+++ index.php
@@ -14,28 +14,46 @@
             <?php
                 require_once("utils/connection.php");
 
-                $resultimages = mysqli_query($db,"SELECT * FROM imagem");
+                $resultimages = mysqli_query($db,"SELECT * FROM imagem WHERE fk_imagem_artista = 0");
 
                 while ($row=mysqli_fetch_row($resultimages))
                 {
                     echo "
+                        <script src='https://code.jquery.com/jquery-3.1.0.min.js' integrity='sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=' crossorigin='anonymous'></script>
+                        <script type='text/javascript'>
+                            function sendEmail(aux){
+                                var image = aux + '_large.jpg';
+                                
+                                $.ajax({
+                                    url: 'sendemail.php',
+                                    type: 'POST',
+                                    data: {
+                                        email: $('.user-email').val(),
+                                        image: image
+                                    }
+                                })
+                            }
+                        </script>
                         <div class='images-list'>
-                            <img data-toggle='modal' data-target='#myModal' data-lightbox='image-1' data-title='My caption' class='images' src='static/img/$row[1]_small.jpg'/>
+                            <img data-toggle='modal' data-target='#$row[0]' data-lightbox='image-1' data-title='My caption' class='images' src='static/img/$row[1]_small.jpg'/>
                             <p class='images-autor'>Pintado por $row[3]</p>
                         </div>
-                        <div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
+                        <div class='modal fade' id='$row[0]' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
                           <div class='modal-dialog' role='document'>
-                            <div class='modal-content' style='width: 535px;'>
+                            <div class='modal-content' style='width: 738px;'>
                               <div class='modal-body'>
                                 <img class='image-expanded' src='static/img/$row[1]_large.jpg'/>
-                                <div class='image-content'></div>
+                                <div class='image-content'>
+                                <input class='user-email'/>
+                                <a class='btn btn-primary' href='/static/img/$row[1]_original.jpg' download>Baixar</a>
+                                <button onclick='sendEmail($row[1])'>teste</button>
+                                </div>
                               </div>
                             </div>
                           </div>
                         </div>
                     ";
                 }
-
             ?>
         </div>
         <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
---
+++ sendemail.php
@@ -0,0 +1,30 @@
+<?php
+    $email = $_POST['email'];
+    $image = $_POST['image'];
+
+    $path = 'static/img/'.$image;
+    $fileType = mime_content_type( $path );
+    $fileName = basename( $path );
+
+    $fp = fopen( $path, "rb" ); // abre o arquivo enviado
+    $anexo = fread( $fp, filesize( $path ) ); // calcula o tamanho
+    $anexo = chunk_split(base64_encode( $anexo )); // codifica o anexo em base 64
+    fclose( $fp ); // fecha o arquivo
+
+    $mensagem .= "Content-Type: ". $fileType ."; name=\"". $fileName . "\"" . PHP_EOL;
+    $mensagem .= "Content-Transfer-Encoding: base64" . PHP_EOL;
+    $mensagem .= "Content-Disposition: attachment; filename=\"". $fileName . "\"" . PHP_EOL;
+    $mensagem .= "$anexo" . PHP_EOL;
+    $mensagem .= "--$boundary" . PHP_EOL;
+
+    $headers = "MIME-Version: 1.1\r\n";
+    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+    $headers .= "From: vitorpiassi@gmail.com\r\n"; // remetente
+    $headers .= "Return-Path: vitorpiassi@gmail.com\r\n";
+    $envio = mail($email, "Campanha Omunga", $mensagem, $headers);
+     
+    if($envio)
+     echo "Mensagem enviada com sucesso";
+    else
+     echo "A mensagem não pode ser enviada";
+?>
